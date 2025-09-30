fb279686e418e69cd4569f9dce3a74ebe22d8b42
Gerysnz <gerardfuentesaf@gmail.com>
2025-09-30 17:40:25 +0200

    exercicis forms acabados

--- ex23pagina1.php
+++ ex23pagina1.php
@@ -5,7 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body> menu desplegable 
+<body style="background-color: 
+<?php 
+        if($_POST['menu'] == 'opcion 1') 
+            echo 'red';
+        elseif($_POST['menu'] == 'opcion 2') 
+            echo 'blue'; 
+        elseif($_POST['menu'] == 'opcion 3') 
+            echo 'green';
+        else echo 'white';
+?>;"> 
     <form action="" method="post">
         <select name="menu" id="">
             
